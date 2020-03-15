70796b1182be72684fcabb41e85c6c844e19ed6f
afonsoafonsoafonso <afonsomendonca91@gmail.com>
2020-03-15 17:55:14 +0000

    function that calculates residential building with best capacity/area ratio

--- building_proj.py
+++ building_proj.py
@@ -1,7 +1,11 @@
 class BuildingProj:
     def __init__(self, type, rows, cols, cenas, plan):
         self.type = type
-        self.rows = rows
-        self.cols = cols
-        self.cenas = cenas
-        self.plan = plan    
+        self.rows = int(rows)
+        self.cols = int(cols)
+        self.cenas = int(cenas)
+        self.plan = plan
+        if type=='R':
+            self.ratio = int(cenas)/(int(rows)*int(cols))
+        else:
+            self.ratio = None
--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from building_proj import BuildingProj
 from building import Building
 #USAGE: main input_file
 
-def parse(file_name, buildings):
+# returns city object. appends building projects to buildings
+def parse_file(file_name, buildings):
     try:
         with open(file_name, 'r') as input_file:
             vars = input_file.readline().split()
             city = City(vars[0], vars[1], vars[2], vars[3])
-
             for line in input_file:
                 plan = []
                 vars = line.split()
@@ -25,11 +25,31 @@ def parse(file_name, buildings):
         print("Error opening or parsing file.")
         raise SystemExit
 
+def get_best_residential(buildingProjs):
+    max = 0
+    n = 0
+    nBest = 0
+    best = None
+    for building in buildingProjs:
+        n = n + 1
+        if building.type=='R':
+            if building.ratio > max:
+                best = building
+                max = building.ratio
+                nBest = n
+    return best,nBest
+
 # main
 file_name = sys.argv[1]
-buildings = []
+buildingProjs = []
 city = None
 
-city = parse(file_name, buildings)
+city = parse_file(file_name, buildingProjs)
 
-print(buildings)
+for building in buildingProjs:
+    print(building.type)
+
+bestResidential, n = get_best_residential(buildingProjs)
+
+print(bestResidential.cenas)
+print(n)
